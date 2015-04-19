6099509e628fb66435ed3045692f717d9445c60d
Mark Dowton <dowton_mark@hotmail.com>
2015-04-20 09:32:22 +1000

    Menu fix and css changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
+		$ = jQuery;
     	var divHeight = jQuery('#works').height();
         console.log(divHeight);
         jQuery('.page-cover').css('height', divHeight);
+        jQuery('.panel-left').css('height', divHeight);
+
+        jQuery('.member-image').on('click', function() {
+        	var href = jQuery(this).attr('href');
+        	jQuery('.member-image').each(function(){
+        		jQuery(this).removeClass('active-member-photo');
+        		if(href == '#markBio'){
+        			jQuery('#photo-mark').addClass("active-member-photo");
+        		} else if (href == '#emmaBio'){
+        			jQuery('#photo-emma-cortson').addClass("active-member-photo");
+        		} else if (href == '#trudyBio') {
+        			jQuery('#photo-trudy').addClass("active-member-photo");
+        		} else if (href == '#hanBio') {
+        			jQuery('#photo-hanwen').addClass("active-member-photo");
+        		} else if (href == '#jonoBio') {
+        			jQuery('#photo-jono').addClass("active-member-photo");
+        		}
+        	});	
+        	
+        });
 });
