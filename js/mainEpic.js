726389f88745b226dc3ead2bc4d1aacc378ac1c1
Mark Dowton <dowton_mark@hotmail.com>
2015-04-21 09:16:56 +1000

    Email fix js fix

--- js/mainEpic.js
+++ js/mainEpic.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
         jQuery('body').on('click', '.member-image', function() {
         	var href = jQuery(this).attr('href');
         	
-        	setTimeout(myEl, 500);
+        	//setTimeout(myEl, 500);
 
 		         
 
